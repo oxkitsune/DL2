526ee10b3ee0ee1011c2e782d36f4126431b610e
Gijs de Jong <berichtaangijs@gmail.com>
2024-05-08 13:28:49 +0200

    residual connections

--- models.py
+++ models.py
@@ -81,50 +81,97 @@ class PatchExpanding(nn.Module):
         return x
 
 
+class WrappedEncoder(nn.Module):
+    def __init__(self, dim: int, depth: int):
+        super().__init__()
+        self.encoder = BasicLayer(dim=dim, depth=depth)
+        self.downsampling = Downsampling(in_channels=dim, out_channels=2 * dim)
+
+    def forward(self, x):
+        residual = self.encoder(x)
+        x = self.downsampling(residual)
+        return x, residual
+
+
+class WrappedDecoder(nn.Module):
+    def __init__(self, dim: int, depth: int):
+        super().__init__()
+        self.decoder = BasicLayer(dim=dim, depth=depth)
+        self.transpose_conv = nn.ConvTranspose3d(
+            in_channels=dim, out_channels=dim // 2, kernel_size=2
+        )
+
+    def forward(self, x, residual):
+        x += residual
+        x = self.decoder(x)
+        x = self.transpose_conv(x)
+        return x
+
+
 class UNet(nn.Module):
     def __init__(self):
         super().__init__()
         # TODO param values of basiclayer?
-        self.encoder1 = BasicLayer(dim=96, depth=2)
-        self.downsampling1 = Downsampling(in_channels=96, out_channels=2 * 96)
-        self.encoder2 = BasicLayer(dim=2 * 96, depth=2)
-        self.downsampling2 = Downsampling(in_channels=2 * 96, out_channels=4 * 96)
-        self.encoder3 = BasicLayer(dim=4 * 96, depth=2)
-        self.downsampling3 = Downsampling(in_channels=4 * 96, out_channels=8 * 96)
-        self.encoder4 = BasicLayer(dim=8 * 96, depth=1)
+        self.encoder1 = WrappedEncoder(dim=96, depth=2)
+        self.encoder2 = WrappedEncoder(dim=2 * 96, depth=2)
+        self.encoder3 = WrappedEncoder(dim=4 * 96, depth=2)
+        self.encoder4 = WrappedEncoder(dim=8 * 96, depth=1)
 
-        self.decoder1 = BasicLayer(dim=8 * 96, depth=2)
-        self.transpose_conv = nn.ConvTranspose3d(
-            in_channels=8 * 96, out_channels=4 * 96, kernel_size=2
-        )
-        self.decoder2 = BasicLayer(dim=4 * 96, depth=2)
-        self.transpose_conv = nn.ConvTranspose3d(
-            in_channels=4 * 96, out_channels=2 * 96, kernel_size=2
-        )
-        self.decoder3 = BasicLayer(dim=2 * 96, depth=2)
-        self.transpose_conv = nn.ConvTranspose3d(
-            in_channels=2 * 96, out_channels=96, kernel_size=2
-        )
-        self.decoder4 = BasicLayer(dim=96, depth=2)
+        self.decoder1 = WrappedDecoder(dim=8 * 96, depth=2)
+        self.decoder2 = WrappedDecoder(dim=4 * 96, depth=2)
+        self.decoder3 = WrappedDecoder(dim=2 * 96, depth=2)
+        self.decoder4 = WrappedDecoder(dim=96, depth=2)
+
+        # self.encoder1 = BasicLayer(dim=96, depth=2)
+        # self.downsampling1 = Downsampling(in_channels=96, out_channels=2 * 96)
+        # self.encoder2 = BasicLayer(dim=2 * 96, depth=2)
+        # self.downsampling2 = Downsampling(in_channels=2 * 96, out_channels=4 * 96)
+        # self.encoder3 = BasicLayer(dim=4 * 96, depth=2)
+        # self.downsampling3 = Downsampling(in_channels=4 * 96, out_channels=8 * 96)
+        # self.encoder4 = BasicLayer(dim=8 * 96, depth=1)
+
+        # self.decoder1 = BasicLayer(dim=8 * 96, depth=2)
+        # self.transpose_conv = nn.ConvTranspose3d(
+        #     in_channels=8 * 96, out_channels=4 * 96, kernel_size=2
+        # )
+        # self.decoder2 = BasicLayer(dim=4 * 96, depth=2)
+        # self.transpose_conv = nn.ConvTranspose3d(
+        #     in_channels=4 * 96, out_channels=2 * 96, kernel_size=2
+        # )
+        # self.decoder3 = BasicLayer(dim=2 * 96, depth=2)
+        # self.transpose_conv = nn.ConvTranspose3d(
+        #     in_channels=2 * 96, out_channels=96, kernel_size=2
+        # )
+        # self.decoder4 = BasicLayer(dim=96, depth=2)
 
     def forward(self, x):
         # TODO residual connections
-        x1 = self.encoder1(x)
-        x1 = self.downsampling1(x1)
-        x2 = self.encoder2(x1)
-        x2 = self.downsampling2(x2)
-        x3 = self.encoder3(x2)
-        x3 = self.downsampling3(x3)
-        x4 = self.encoder4(x3)
+        x, residual1 = self.encoder1(x)
+        x, residual2 = self.encoder2(x)
+        x, residual3 = self.encoder3(x)
+        x, residual4 = self.encoder4(x)
 
-        y1 = self.decoder1(x4)
-        y1 = self.transpose_conv(y1)
-        y2 = self.decoder2(y1)
-        y2 = self.transpose_conv(y2)
-        y3 = self.decoder3(y2)
-        y3 = self.transpose_conv(y3)
-        y4 = self.decoder4(y3)
-        return y4
+        x = self.decoder1(x, residual4)
+        x = self.decoder2(x, residual3)
+        x = self.decoder3(x, residual2)
+        x = self.decoder4(x, residual1)
+
+        # x1 = self.encoder1(x)
+        # x1 = self.downsampling1(x1)
+        # x2 = self.encoder2(x1)
+        # x2 = self.downsampling2(x2)
+        # x3 = self.encoder3(x2)
+        # x3 = self.downsampling3(x3)
+        # x4 = self.encoder4(x3)
+
+        # y1 = self.decoder1(x4)
+        # y1 = self.transpose_conv(y1)
+        # y2 = self.decoder2(y1)
+        # y2 = self.transpose_conv(y2)
+        # y3 = self.decoder3(y2)
+        # y3 = self.transpose_conv(y3)
+        # y4 = self.decoder4(y3)
+        return x
 
 
 if __name__ == "__main__":
