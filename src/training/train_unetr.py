c7676dff71c1ea88d153451d91b0e28a9c7d6932
Gijs de Jong <berichtaangijs@gmail.com>
2024-05-16 11:32:34 +0200

    add progress bar

--- src/training/train_unetr.py
+++ src/training/train_unetr.py
@@ -1,31 +1,35 @@
 import torch
+from tqdm import tqdm
+
 
 def train_unetr(data_loader, model, epochs, ptv_index):
-    
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     print(f"Using device {device}")
-    criterion = torch.nn.L1Loss() 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001) # use lr 1e-4 and AdamW
-    
+    criterion = torch.nn.L1Loss()
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=0.0001
+    )  # use lr 1e-4 and AdamW
+
     model.to(device)
     model.train()
-    
-    for epoch in range(epochs):
+
+    trange = tqdm(range(epochs), desc="Training model")
+
+    for epoch in trange:
         data_loader.shuffle_data()
-        
-        for batch in data_loader.get_batches():
+
+        subtrange = tqdm(data_loader.get_batches(), desc="Train", leave=False)
+
+        for batch in subtrange:
             features = batch.get_flattend_oar_features(ptv_index=ptv_index)
             input = torch.Tensor(features).transpose(1, 4).to(device)
             target = batch.dose
             target = torch.Tensor(target).transpose(1, 4).to(device)
-            
-            print(input.shape)
-            print(target.shape)
-            
+
             optimizer.zero_grad()
             outputs = model(input)
             loss = criterion(outputs, target)
             loss.backward()
             optimizer.step()
-            
-            print(f"Model loss at epoch {epoch} is {loss.item():.3f}")
+
+            trange.write(f"Model loss at epoch {epoch} is {loss.item():.3f}")
