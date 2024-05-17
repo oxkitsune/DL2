6dc1e83b1ec84ff5c54d19f5eb9b61d016fe1707
Gijs de Jong <berichtaangijs@gmail.com>
2024-05-17 22:53:20 +0200

    don't pin memory

--- src/training/train_unetr.py
+++ src/training/train_unetr.py
@@ -15,9 +15,7 @@ def train_unetr(dataset, args):
     criterion = torch.nn.L1Loss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
 
-    train_dataloader = DataLoader(
-        dataset["train"], batch_size=args.batch_size, pin_memory=True
-    )
+    train_dataloader = DataLoader(dataset["train"], batch_size=args.batch_size)
     dev_data_loader = DataLoader(dataset["validation"], batch_size=args.batch_size)
 
     pbar = tqdm(range(args.epochs), desc="Training model")
