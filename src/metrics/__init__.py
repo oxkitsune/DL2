0b0a048b62bbd289c9098ed803ccb3d3762dcc9d
Gijs de Jong <berichtaangijs@gmail.com>
2024-05-22 19:41:10 +0200

    AHHHH

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -69,6 +69,9 @@ def dvh_score_for_single_prediction(prediction, voxel_dims, structure_masks):
     metrics = {k: {} for k in ALL_ROIS}
     for roi_index, roi in enumerate(ALL_ROIS):
         roi_mask = structure_masks[..., roi_index].to(torch.bool)
+
+        print(roi_mask.shape)
+        print(roi_mask[0, 0, 0])
         if not roi_mask.any():
             continue
 
