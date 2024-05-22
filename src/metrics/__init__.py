de71fe9f31c484f5e389dae85563a39112ae686c
Gijs de Jong <berichtaangijs@gmail.com>
2024-05-22 16:39:11 +0200

    AHHHH

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -62,8 +62,8 @@ def dvh_score_for_single_prediction(prediction, voxel_dims, structure_masks):
     )
     metrics = {}
     for roi_index, roi in enumerate(ALL_ROIS):
-        roi_mask = structure_masks[..., roi_index].to(torch.bool).unsqueeze(0)
-        roi_dose = prediction[roi_mask]
+        roi_mask = structure_masks[..., roi_index].to(torch.bool)
+        roi_dose = prediction.squeeze()[roi_mask]
         roi_size = roi_dose.size(0)
 
         for metric in ALL_DVH_METRICS[roi]:
